a78150f0bbaefc46974a314441b6be2f27159092
Paul Squires <planetsquires@gmail.com>
2023-09-08 12:30:53 -0230

    "Wait" message also added to Roll transactions.

--- IB-Tracker/src/TradeDialog/TradeDialogSave.cpp
+++ IB-Tracker/src/TradeDialog/TradeDialogSave.cpp
@@ -460,7 +460,7 @@ void TradeDialog_CreateOptionsTradeData(HWND hwnd)
             leg->original_quantity = intQuantity;
             leg->open_quantity = intQuantity;
             if (tws_IsConnected()) {
-                leg->average_price_text = L"Wait..";    // visual notice until next price update arrives.
+                leg->average_price_text = L"Wait...";    // visual notice until next price update arrives.
             }
             break;
 
@@ -472,6 +472,9 @@ void TradeDialog_CreateOptionsTradeData(HWND hwnd)
             if (!tdd.legs.empty()) {
                 tdd.legs.at(row)->open_quantity += guiData.legs.at(row).original_quantity;
                 leg->leg_back_pointer_id = tdd.legs.at(row)->leg_id;
+            }
+            if (tws_IsConnected()) {
+                leg->average_price_text = L"Wait...";    // visual notice until next price update arrives.
             }
             break;
         }
