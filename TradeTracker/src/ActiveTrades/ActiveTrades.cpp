e641eca52eb99187d2ab7284fbc241e199f0b806
Paul Squires <planetsquires@gmail.com>
2024-01-08 09:25:56 -0330

    More leg calculation changes (still not working 100%)

--- TradeTracker/src/ActiveTrades/ActiveTrades.cpp
+++ TradeTracker/src/ActiveTrades/ActiveTrades.cpp
@@ -315,7 +315,9 @@ void CActiveTrades::UpdateLegPortfolioLine(int index, ListBoxData* ld) {
         ld->SetTextData(COLUMN_TICKER_PORTFOLIO_1, text, theme_color);   // Book Value and average Price
 
         // MARKET VALUE
-        double market_value = (pd.market_price * ld->leg->open_quantity * ld->trans->multiplier);
+        double multiplier = 100;
+        if (ld->trans) multiplier = ld->trans->multiplier;
+        double market_value = (pd.market_price * ld->leg->open_quantity * multiplier);
         ld->leg->market_value = market_value;
         text = AfxMoney(market_value, true, ld->trade->ticker_decimals);
         ld->leg->market_value_text = text;
