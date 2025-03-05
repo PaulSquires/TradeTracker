022d1894e49ff203f60f4256016dc71a54663b29
Paul Squires <planetsquires@gmail.com>
2025-03-04 21:41:06 -0330

    20250304

--- cmake_build.bat
+++ cmake_build.bat
@@ -2,8 +2,15 @@
 
 REM This batch file must be in same folder as CMakeLists.txt
 
-rem @call %comspec% /k "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
-rem call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+set __VCVARSALL_TARGET_ARCH=x64
+set __VCVARSALL_HOST_ARCH=x64
+set PATH=%PATH%;C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.42.34433\bin\Hostx64\x64
+
+
+CD %~dp0
+SET build_dir=build
+rem Visual Studio generator does not support generating compile_commands.json
+rem The Ninja generator produces the json file but generates other errors on Windows
 
 
 REM *************************
@@ -11,8 +18,7 @@ REM    CONFIGURE AND BUILD
 REM *************************
 CD %~dp0
 SET build_dir=build
-cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -S. -B%build_dir% -GNinja
-
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -S. -G "Visual Studio 17 2022" -B%build_dir%
 IF "%~2"=="RELEASE" (cmake --build %build_dir% -j) ELSE (cmake --build %build_dir% -j --config Release)
 IF %ERRORLEVEL% NEQ 0 (GOTO BUILD_FAILED) 
 
@@ -20,8 +26,9 @@ IF %ERRORLEVEL% NEQ 0 (GOTO BUILD_FAILED)
 REM *************************
 REM     RUN APPLICATION
 REM *************************
+rem IF "%~2"=="RELEASE" (cmake --build %build_dir% -j) ELSE (cmake --build %build_dir% -j --config Release)
 IF "%~1"=="" exit 0
-SET app_name=%~dp0%build_dir%\%1.exe
+SET app_name=%~dp0%build_dir%\Release\%1.exe
 ECHO Run application: %app_name%
 START %app_name%
 EXIT 0
